f8c594e3484f6e972d32797ac24890f2762741c5
yann <yanndubois4@gmail.com>
2023-05-24 17:17:29 +0200

    feat: add user's location

--- app/components/App.tsx
+++ app/components/App.tsx
@@ -1,26 +1,87 @@
 "use client";
+import React, { useEffect, useState } from "react";
+import dynamic from "next/dynamic";
+import { MapContainer, TileLayer, Marker, Polyline } from "react-leaflet";
 
-import React, { useEffect } from "react";
+const DynamicMapContainer = dynamic(
+  () => import("react-leaflet").then((module) => module.MapContainer),
+  {
+    ssr: false,
+  }
+);
 
 const Map: React.FC = () => {
+  const [userLocation, setUserLocation] = useState<[number, number] | null>(
+    null
+  );
+  const [tracking, setTracking] = useState(false);
+  const [journey, setJourney] = useState<[number, number][]>([]);
+
   useEffect(() => {
-    import("leaflet").then((L) => {
-      const map = L.map("map").setView([51.505, -0.09], 13);
+    if (tracking) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          const newPosition: [number, number] = [latitude, longitude];
+          setJourney((prevJourney) => [...prevJourney, newPosition]);
+          setUserLocation(newPosition);
+        },
+        (error) => {
+          console.log("Error getting user location:", error);
+        },
+        {
+          enableHighAccuracy: true,
+          timeout: 5000,
+        }
+      );
+    }
+  }, [tracking]);
 
-      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        attribution:
-          'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-      }).addTo(map);
+  const handleStartTracking = () => {
+    setJourney([]);
+    setTracking(true);
+  };
 
-      L.marker([51.505, -0.09]).addTo(map).bindPopup("A sample marker.");
+  const handleStopTracking = () => {
+    setTracking(false);
+  };
 
-      return () => {
-        map.remove();
-      };
-    });
-  }, []);
-
-  return <div id='map' style={{ height: "400px" }}></div>;
+  return (
+    <div>
+      <div id='map' style={{ height: "400px" }}>
+        {typeof window !== "undefined" && (
+          <DynamicMapContainer
+            center={[51.505, -0.09]}
+            zoom={13}
+            style={{ height: "100%" }}
+          >
+            {userLocation && <Marker position={userLocation} />}
+            {journey.length > 1 && (
+              <Polyline positions={journey} color='lime' />
+            )}
+            <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
+          </DynamicMapContainer>
+        )}
+      </div>
+      <div>
+        {tracking ? (
+          <button onClick={handleStopTracking}>Stop Tracking</button>
+        ) : (
+          <button onClick={handleStartTracking}>Start Tracking</button>
+        )}
+      </div>
+      <div>
+        <h2>Journey</h2>
+        <ul>
+          {journey.map((position, index) => (
+            <li key={index}>
+              Latitude: {position[0]}, Longitude: {position[1]}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Map;
